acbbd16e6fddbe20f7bec4e0b7611b60b9cc6cae
7k7k-Qin1 <tqinaf@connect.ust.hk>
2023-06-05 15:48:32 +0800

    fix: export

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+export { AppController } from './app.controller';
+export { AppService } from './app.service';
+export { LoggerMiddleware } from './logger.middleware';
+export { CacheService } from './cache.service';
